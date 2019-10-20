193979884a888a52338a47c546db4e9a8b611064
ambika <abhi.fullstackdeveloper@gmail.com>
2019-10-20 21:33:06 +0530

    some changes

---
+++ src/Admin/Layout/Backend/Backend.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { mainListItems, secondaryListItems } from '../../Element/Header/Header'
+import Footer from '../../Element/Footer/Footer';
+import useStyles  from '../../Hooks/useStyle';
+import clsx from 'clsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Badge from '@material-ui/core/Badge';
+import Typography from '@material-ui/core/Typography';
+
+
+const Layout = props => {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+    const handleDrawerOpen = () => {
+        console.log('open value '+ open)
+      setOpen(true);
+    };
+    const handleDrawerClose = () => {
+      console.log('close value '+open)
+      setOpen(false);
+    };
+    
+    return (
+        <React.Fragment>
+            <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="absolute" className={clsx(classes.appBar, !open && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                    Dashboard
+                </Typography>
+                <IconButton color="inherit">
+                    <Badge badgeContent={4} color="secondary">
+                    <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                }}
+                open={open}
+            >
+                {open?(<div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>):(<IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                >
+                    <MenuIcon />
+                </IconButton>)}
+                
+                
+                <Divider />
+                <List>{mainListItems}</List>
+                <Divider />
+                <List>{secondaryListItems}</List>
+            </Drawer>
+            
+                <main className={classes.content}>
+                    <div className={classes.appBarSpacer} />
+                    {props.children}
+                    <Footer />
+                </main>
+            </div>
+    </React.Fragment>
+    )
+}
+
+export default Layout;
