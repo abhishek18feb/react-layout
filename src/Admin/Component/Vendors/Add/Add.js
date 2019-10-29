6cc929ec8fa7fadd2b1fccb907b778e6ea31a121
ambika <abhi.fullstackdeveloper@gmail.com>
2019-10-29 13:35:41 +0530

    new changes

--- src/Admin/Component/Vendors/Add/Add.js
+++ src/Admin/Component/Vendors/Add/Add.js
@@ -18,6 +18,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Select from 'react-select';
+import ContactPersonsComp from '../ContactPersons';
 import './Add.css';
 
 
@@ -316,84 +317,14 @@ const Add = props =>{
                                         <TableBody>
                                         {
                                             contactPersonState.map((val, idx) => {
-                                                const salut = `Salutation-${idx}`;
-                                                const firstName = `firstName-${idx}`;
-                                                const lastName = `lastName-${idx}`;
-                                                const email = `email-${idx}`;
-                                                const workPhone = `workPhone-${idx}`;
-                                                const mobile = `mobile-${idx}`;
-                                                return (
-                                                <TableRow key={`cat-${idx}`}>
-                                                    <TableCell component="th" scope="row">
-                                                        <TextField
-                                                                id={salut}
-                                                                name={salut}
-                                                                dataset={idx}
-                                                                select
-                                                                label="Salutation"
-                                                                className="Salutation"
-                                                                value={contactPersonState[idx].Salutation}
-                                                                onChange={handleContactPersonChange}
-                                                                SelectProps={{
-                                                                MenuProps: {
-                                                                    className: classes.menu,
-                                                                },
-                                                                }}
-                                                                helperText="Please select Salutation"
-                                                                margin="normal"
-                                                                fullWidth
-                                                            >{salutation.map(option => (
-                                                                <MenuItem key={option.value} value={option.value}>
-                                                                {option.label}
-                                                                </MenuItem>
-                                                            ))}
-                                                        </TextField>
-                                                    </TableCell>
-                                                    <TableCell component="th" scope="row">
-                                                        <TextField required name={firstName} 
-                                                                    className="firstName"
-                                                                    dataset={idx} 
-                                                                    id={firstName} label="First Name" 
-                                                                    value={contactPersonState[idx].firstName}
-                                                                    onChange={handleContactPersonChange}  fullWidth />
-                                                    </TableCell>
-                                                    <TableCell align="right">
-                                                        <TextField required name={lastName} 
-                                                                    className="lastName"
-                                                                    data-idx={idx} 
-                                                                    id={lastName} label="Last Name" 
-                                                                    value={contactPersonState[idx].lastName}
-                                                                    onChange={handleContactPersonChange}  fullWidth />
-                                                    </TableCell>
-                                                    <TableCell align="right">
-                                                                <TextField required name={email} 
-                                                                    className="email"
-                                                                    data-idx={idx} 
-                                                                    id={email} label="Email" 
-                                                                    value={contactPersonState[idx].email}
-                                                                    onChange={handleContactPersonChange}  fullWidth />
-                                                    </TableCell>
-                                                    <TableCell align="right">
-                                                        <TextField required name={workPhone} 
-                                                                    className="workPhone"
-                                                                    data-idx={idx} 
-                                                                    id={workPhone} label="Work Phone" 
-                                                                    value={contactPersonState[idx].workPhone}
-                                                                    onChange={handleContactPersonChange}  fullWidth />
-                                                        </TableCell>
-                                                    <TableCell align="right">
-                                                        <TextField required name={mobile} 
-                                                            className="mobile"
-                                                            data-idx={idx} 
-                                                            id={mobile} label="Mobile" 
-                                                            value={contactPersonState[idx].mobile}
-                                                            onChange={handleContactPersonChange}  fullWidth />
-                                                    </TableCell>
-                                                    <TableCell><input type="button" value="Remove Person" onClick={(index)=>removeContactPerson(idx)} /></TableCell>
-                                                </TableRow>
-                                                );      
+                                                return (<ContactPersonsComp key={`cat-${idx}`}
+                                                    idx={idx} 
+                                                    contactPersonState={contactPersonState} 
+                                                    handleContactPersonChange={handleContactPersonChange} 
+                                                    removeContactPerson={removeContactPerson}
+                                                    salutation={salutation} />)
                                             })
-                                            }
+                                        }
                                            
                                         </TableBody>
                                     </Table>
--- src/Admin/Component/Vendors/ContactPersons.js
+++ src/Admin/Component/Vendors/ContactPersons.js
@@ -1,79 +1,93 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import TextField from '@material-ui/core/TextField';
+import useStyles from '../../Hooks/useStyle';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const ContactPersons = ({idx, contactPersons, handleContactPersonChange}) =>{
+const ContactPersonsComp = ({idx, contactPersonState, handleContactPersonChange, removeContactPerson, salutation}) =>{
+    const salut = `Salutation-${idx}`;
+    const firstName = `firstName-${idx}`;
+    const lastName = `lastName-${idx}`;
+    const email = `email-${idx}`;
+    const workPhone = `workPhone-${idx}`;
+    const mobile = `mobile-${idx}`;
+    const classes = useStyles();
     return (
         <React.Fragment>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Salutation</TableCell>
-                        <TableCell align="right">First Name</TableCell>
-                        <TableCell align="right">Last Name</TableCell>
-                        <TableCell align="right">Email Address</TableCell>
-                        <TableCell align="right">Work Phone</TableCell>
-                        <TableCell align="right">Mobile</TableCell>
-                        <TableCell align="right">X</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {
-                    contactPersonState.map((val, idx) => {
-                        const salutation = `Salutation-${idx}`;
-                        const firstName = `firstName-${idx}`;
-                        const lastName = `lastName-${idx}`;
-                        const email = `email-${idx}`;
-                        const workPhone = `workPhone-${idx}`;
-                        const mobile = `mobile-${idx}`;
-                        return (
-                        <TableRow key={`cat-${idx}`}>
-                            <TableCell component="th" scope="row">
-                                <TextField
-                                        id={salutation}
-                                        name={salutation}
-                                        data-idx={idx}
-                                        select
-                                        label="Salutation"
-                                        className="Salutation"
-                                        value={contactPersons[idx].Salutation}
-                                        onChange={handleContactPersonChange}
-                                        SelectProps={{
-                                        MenuProps: {
-                                            className: classes.menu,
-                                        },
-                                        }}
-                                        helperText="Please select Salutation"
-                                        margin="normal"
-                                        fullWidth
-                                    >{salutation.map(option => (
-                                        <MenuItem key={option.value} value={option.value}>
-                                        {option.label}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                            </TableCell>
-                            <TableCell component="th" scope="row">
-                                <TextField required name={firstName} 
-                                            className="firstName"
-                                            data-idx={idx} 
-                                            id={firstName} label="First Name" 
-                                            value={contactPersons[idx].firstName}
-                                            onChange={handleContactPersonChange}  fullWidth />
-                            </TableCell>
-                            <TableCell align="right">a</TableCell>
-                            <TableCell align="right">a</TableCell>
-                            <TableCell align="right">j</TableCell>
-                            <TableCell align="right">o</TableCell>
-                        
-                        </TableRow>
-                        );      
-                    })
-                    }
+              <TableRow key={`cat-${idx}`}>
+                    <TableCell component="th" scope="row">
+                        <TextField
+                                id={salut}
+                                name={salut}
+                                dataset={idx}
+                                select
+                                label="Salutation"
+                                className="Salutation"
+                                value={contactPersonState[idx].Salutation}
+                                onChange={handleContactPersonChange}
+                                SelectProps={{
+                                MenuProps: {
+                                    className: classes.menu,
+                                },
+                                }}
+                                helperText="Please select Salutation"
+                                margin="normal"
+                                fullWidth
+                            >{salutation.map(option => (
+                                <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                        <TextField required name={firstName} 
+                                    className="firstName"
+                                    dataset={idx} 
+                                    id={firstName} label="First Name" 
+                                    value={contactPersonState[idx].firstName}
+                                    onChange={handleContactPersonChange}  fullWidth />
+                    </TableCell>
+                    <TableCell align="right">
+                        <TextField required name={lastName} 
+                                    className="lastName"
+                                    data-idx={idx} 
+                                    id={lastName} label="Last Name" 
+                                    value={contactPersonState[idx].lastName}
+                                    onChange={handleContactPersonChange}  fullWidth />
+                    </TableCell>
+                    <TableCell align="right">
+                                <TextField required name={email} 
+                                    className="email"
+                                    data-idx={idx} 
+                                    id={email} label="Email" 
+                                    value={contactPersonState[idx].email}
+                                    onChange={handleContactPersonChange}  fullWidth />
+                    </TableCell>
+                    <TableCell align="right">
+                        <TextField required name={workPhone} 
+                                    className="workPhone"
+                                    data-idx={idx} 
+                                    id={workPhone} label="Work Phone" 
+                                    value={contactPersonState[idx].workPhone}
+                                    onChange={handleContactPersonChange}  fullWidth />
+                        </TableCell>
+                    <TableCell align="right">
+                        <TextField required name={mobile} 
+                            className="mobile"
+                            data-idx={idx} 
+                            id={mobile} label="Mobile" 
+                            value={contactPersonState[idx].mobile}
+                            onChange={handleContactPersonChange}  fullWidth />
+                    </TableCell>
+                    <TableCell><input type="button" value="Remove Person" onClick={(index)=>removeContactPerson(idx)} /></TableCell>
+                </TableRow>
                     
-                </TableBody>
-            </Table>
+                                            
         </React.Fragment>
     )
 }
 
-export default ContactPersons;
+export default ContactPersonsComp;
